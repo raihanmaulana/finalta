3d1273db91b69e95071b3cf6e9c209095d06974e
raihanmaulana <raihanmaulana09@gmail.com>
2024-01-29 22:42:35 +0700

    ini biar login ga ngebug

--- resources/views/auth/login-anggota.blade.php
+++ resources/views/auth/login-anggota.blade.php
@@ -3,16 +3,16 @@
 
 <head>
     <title>Login Anggota Perpustakaan</title>
+
     <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="public\css\dashboard.css">
-</head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="public\css\dashboard.css">
+    </head>
 
 
-<body>
+    <!-- <body>
     <h2>Login Anggota Perpustakaan</h2>
     @if ($errors->any())
     <div class="alert alert-danger">
@@ -38,9 +38,9 @@
         <a class="btn btn-outline-primary" href="anggota/register" role="button">Register</a>
         <ul>
     </form>
-</body>
-@extends('account.layout')
-@section('index')
+</body> -->
+    @extends('account.layout')
+    @section('index')
     <div class="page page1">
         <div class="container d-flex justify-content-center align-items-center vh-100">
             <div class="col-12 col-md-8 col-lg-4">
@@ -58,20 +58,17 @@
                                     @csrf
                                     <div class="mb-3">
                                         <label for="nomor_anggota" class="form-label">Nomor Anggota</label>
-                                        <input type="text" class="form-control" id="nomor_anggota" name="nomor_anggota"
-                                            placeholder="Masukkan Nomor Anggota Anda"required>
+                                        <input type="text" class="form-control" id="nomor_anggota" name="nomor_anggota" placeholder="Masukkan Nomor Anggota Anda" required>
                                     </div>
                                     <div class="mb-3">
                                         <label for="password" class="form-label">Kata Sandi</label>
-                                        <input type="password" class="form-control" id="password" name="password" required
-                                            placeholder="Masukkan Kata Sandi Anda" />
+                                        <input type="password" class="form-control" id="password" name="password" required placeholder="Masukkan Kata Sandi Anda" />
                                     </div>
                                     <div class="module-foot">
                                         <div class="control-group">
                                             <div class="controls clearfix">
                                                 <div class="row-6 text-center center-content">
-                                                    <button type="submit"
-                                                        class="btn btn-dark btn-lg btn-block">Login</button>
+                                                    <button type="submit" class="btn btn-dark btn-lg btn-block">Login</button>
                                                 </div>
                                                 <label class="checkbox">
                                                     <input type="checkbox" name="remember" id="remember"> Remember me
@@ -91,4 +88,4 @@
 
 
 
-@stop
+    @stop
