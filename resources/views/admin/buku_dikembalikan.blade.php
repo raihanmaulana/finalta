06779d56d60f21d3ff6cd23e0ed456dfa7128587
joanneevd <joanneedt@gmail.com>
2024-01-29 20:28:47 +0700

    peminjaman

--- resources/views/admin/buku_dikembalikan.blade.php
+++ resources/views/admin/buku_dikembalikan.blade.php
@@ -1,34 +1,39 @@
-<!-- resources/views/admin/buku_dikembalikan.blade.php -->
-
-@extends('layouts.app')
+@extends('layout.index')
+@section('custom_top_script')
+@stop
 
 @section('content')
-<div class="container">
-    <h2>Buku yang Dikembalikan</h2>
-
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Nama Anggota</th>
-                <th>Judul Buku</th>
-                <th>Tanggal Pengembalian</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse($bukuDikembalikan as $index => $item)
-            <tr>
-                <td>{{ $index + 1 }}</td>
-                <td>{{ optional($item->anggota)->nama_anggota }}</td>
-                <td>{{ optional($item->buku)->judul_buku }}</td>
-                <td>{{ $item->created_at }}</td>
-            </tr>
-            @empty
-            <tr>
-                <td colspan="4">Tidak ada buku yang dikembalikan.</td>
-            </tr>
-            @endforelse
-        </tbody>
-    </table>
-</div>
+    <div class="content">
+        <div class="module">
+            <div class="module-head">
+                <h3>Riwayat Peminjaman</h3>
+            </div>
+            <div class="module-body">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama Anggota</th>
+                            <th>Judul Buku</th>
+                            <th>Tanggal Pengembalian</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($bukuDikembalikan as $index => $item)
+                            <tr>
+                                <td>{{ $index + 1 }}</td>
+                                <td>{{ optional($item->anggota)->nama_anggota }}</td>
+                                <td>{{ optional($item->buku)->judul_buku }}</td>
+                                <td>{{ $item->created_at }}</td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="4">Tidak ada buku yang dikembalikan.</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/admin/peminjaman.blade.php
+++ resources/views/admin/peminjaman.blade.php
@@ -1,58 +1,63 @@
-<!-- resources/views/admin/daftar-peminjaman.blade.php -->
-
-@extends('layouts.app')
+@extends('layout.index')
+@section('custom_top_script')
+@stop
 
 @section('content')
-<div class="container">
-    <h2>Daftar Permintaan Peminjaman</h2>
+    <div class="content">
+        <div class="module">
+            <div class="module-head">
+                <h3>Konfirmasi Peminjaman</h3>
+            </div>
+            <div class="module-body">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>ID Anggota</th>
+                            <th>Nama Anggota</th>
+                            <th>Judul Buku</th>
+                            <th>Status</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- Tampilkan daftar permintaan peminjaman -->
+                        @forelse($permintaanPeminjaman as $index => $peminjaman)
+                            <tr>
+                                <td>{{ $index + 1 }}</td>
+                                <td>{{ $peminjaman->anggota->id_anggota ?? 'Default ID' }}</td>
+                                <td>{{ $peminjaman->anggota->nama_anggota ?? 'Default Name' }}</td>
+                                <td>{{ $peminjaman->buku->judul_buku ?? 'Default Title' }}</td>
 
-    <!-- Daftar Permintaan Peminjaman untuk Disetujui -->
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>ID Anggota</th>
-                <th>Nama Anggota</th>
-                <th>Judul Buku</th>
-                <th>Status</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            <!-- Tampilkan daftar permintaan peminjaman -->
-            @forelse($permintaanPeminjaman as $index => $peminjaman)
-            <tr>
-                <td>{{ $index + 1 }}</td>
-                <td>{{ $peminjaman->anggota->id_anggota ?? 'Default ID' }}</td>
-                <td>{{ $peminjaman->anggota->nama_anggota ?? 'Default Name' }}</td>
-                <td>{{ $peminjaman->buku->judul_buku ?? 'Default Title' }}</td>
-
-                <td>
-                    @if($peminjaman->status == 0)
-                    'Pending'
-                    @else
-                    'Approved'
-                    @endif
-                </td>
-                <td>
-                    @if($peminjaman->status == 0)
-                    <!-- Tombol Setujui -->
-                    <form action="{{ route('admin.peminjaman.approve', $peminjaman->id) }}" method="POST">
-                        @csrf
-                        @method('PUT')
-                        <button type="submit" class="btn btn-success">Setujui</button>
-                    </form>
-                    @else
-                    <span class="text-success">Sudah Disetujui</span>
-                    @endif
-                </td>
-            </tr>
-            @empty
-            <tr>
-                <td colspan="6">Tidak ada permintaan peminjaman.</td>
-            </tr>
-            @endforelse
-        </tbody>
-    </table>
-</div>
+                                <td>
+                                    @if ($peminjaman->status == 0)
+                                        'Pending'
+                                    @else
+                                        'Approved'
+                                    @endif
+                                </td>
+                                <td>
+                                    @if ($peminjaman->status == 0)
+                                        <!-- Tombol Setujui -->
+                                        <form action="{{ route('admin.peminjaman.approve', $peminjaman->id) }}"
+                                            method="POST">
+                                            @csrf
+                                            @method('PUT')
+                                            <button type="submit" class="btn btn-success">Setujui</button>
+                                        </form>
+                                    @else
+                                        <span class="text-success">Sudah Disetujui</span>
+                                    @endif
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="6">Tidak ada permintaan peminjaman.</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
