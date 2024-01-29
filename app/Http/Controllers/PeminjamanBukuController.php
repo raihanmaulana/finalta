577b2e3b4ab4cd689635cd7dc966d5f9f92fbfdb
raihanmaulana <raihanmaulana09@gmail.com>
2024-01-29 16:22:03 +0700

    udah gua hapus controllernya

--- app/Http/Controllers/PeminjamanBukuController.php
+++ app/Http/Controllers/PeminjamanBukuController.php
@@ -43,7 +43,6 @@ class PeminjamanBukuController extends Controller
 
     public function kembalikanBukuAnggota($id)
     {
-        // Mencari peminjaman berdasarkan ID
         $peminjaman = PeminjamanBuku::findOrFail($id);
 
         // Memastikan status peminjaman masih pending (status = 0)
@@ -51,11 +50,8 @@ class PeminjamanBukuController extends Controller
             return redirect()->route('admin.buku-dipinjam')->with('error', 'Buku tidak dapat dikembalikan karena status peminjaman belum disetujui.');
         }
 
-
         // Mengembalikan stok buku setelah buku dikembalikan
         $buku = $peminjaman->buku;
-        $buku->stok += 1;
-        $buku->save();
 
         // Mengupdate status peminjaman menjadi dikembalikan
         $peminjaman->update(['status' => 2]);
@@ -90,7 +86,7 @@ class PeminjamanBukuController extends Controller
             $buku = Buku::findOrFail($peminjaman->id_buku);
 
             // Mengurangi stok buku setelah permintaan disetujui
-            $buku->stok -= 1;
+            $buku->tersedia -= 1;
             $buku->save();
 
             // Menyetujui permintaan peminjaman
@@ -143,6 +139,8 @@ class PeminjamanBukuController extends Controller
                 'status' => 1,
             ]);
 
+            $buku->tersedia -= 1;
+            $buku->save();
             return redirect()->back()->with('success', 'Buku berhasil dipinjam.');
         } else {
             return redirect()->back()->with('error', 'Nomor anggota atau nomor buku tidak valid.');
