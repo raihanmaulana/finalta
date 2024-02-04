dd51462c65de9c0c4ef06df2bd1a32471c9c7d85
joanneevd <joanneedt@gmail.com>
2024-02-04 19:30:42 +0700

    update profile anggota

--- resources/views/anggota/profile.blade.php
+++ resources/views/anggota/profile.blade.php
@@ -1,24 +1,27 @@
-<!-- resources/views/admin/profile.blade.php -->
-
 @extends('anggota.index')
 @section('custom_top_script')
 @stop
 
 @section('content')
-<h1>Profil Anggota: {{ $user->nama_anggota }}</h1>
-<p>Nomor Anggota: {{ $user->nomor_anggota }}</p>
-<p>Email: {{ $user->email }}</p>
-<p>Jurusan: {{ $user->jurusan }}</p>
-<p>Kelas: {{ $user->kelas }}</p>
+    <div class="content">
+        <div class="module">
+            <div class="module-head">
+                <h3>Profile Anggota</h3>
+            </div>
+            <div class="module-body">
 
-<a href="{{ route('anggota.profile.change-password') }}">Ganti Kata Sandi</a>
-@stop
+                <h2>{{ $user->nama_anggota }}</h2>
+                <p>Nomor Anggota: {{ $user->nomor_anggota }}</p>
+                <p>Email: {{ $user->email }}</p>
+                <p>Jurusan: {{ $user->jurusan }}</p>
+                <p>Kelas: {{ $user->kelas }}</p>
 
+                <a href="{{ route('anggota.profile.change-password') }}">Ganti Kata Sandi</a>
+
+
+
+            </div>
+        </div>
+    </div>
+@endsection
 @section('custom_bottom_script')
-<script type="text/javascript">
-    var kategori_list = $('#kategori_list').val();
-</script>
-<script type="text/javascript" src="{{ asset('static/custom/js/script.addbook.js') }}"></script>
-<script type="text/template" id="allbooks_show">
-    @include('underscore.allbooks_show')
-    @stop
