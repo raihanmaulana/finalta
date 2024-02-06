396403ddbc42a5581a12a07310ecab0b074a1667
raihanmaulana <raihanmaulana09@gmail.com>
2024-02-06 15:40:55 +0700

    udah bvisa gambar

--- resources/views/panel/addbook.blade.php
+++ resources/views/panel/addbook.blade.php
@@ -10,58 +10,50 @@
             <h3>Tambah Buku</h3>
         </div>
         <div class="module-body">
-            <form class="form-horizontal row-fluid" method="POST" action="{{ route('book.store') }}">
+            <form class="form-horizontal row-fluid" method="POST" action="{{ route('book.store') }}" enctype="multipart/form-data">
                 @csrf
                 <div class="control-group">
                     <label class="control-label">Judul Buku</label>
                     <div class="controls">
-                        <input type="text" name="judul_buku" id="judul_buku" data-form-field="judul_buku" placeholder="Masukkan Judul Buku" class="span8">
+                        <input type="text" name="judul_buku" id="judul_buku" placeholder="Masukkan Judul Buku" class="span8">
                     </div>
                 </div>
 
                 <div class="control-group">
                     <label class="control-label">ISBN</label>
                     <div class="controls">
-                        <input type="text" name="nomor_buku" id="nomor_buku" data-form-field="nomor_buku" placeholder="Masukkan ISBN" class="span8">
+                        <input type="text" name="nomor_buku" id="nomor_buku" placeholder="Masukkan ISBN" class="span8">
                     </div>
                 </div>
 
                 <div class="control-group">
                     <label class="control-label">Penerbit</label>
                     <div class="controls">
-                        <input type="text" name="penerbit" id="penerbit" data-form-field="penerbit" placeholder="Masukkan Nama Penerbit" class="span8">
+                        <input type="text" name="penerbit" id="penerbit" placeholder="Masukkan Nama Penerbit" class="span8">
                     </div>
                 </div>
                 <div class="control-group">
                     <label class="control-label">Pengarang</label>
                     <div class="controls">
-                        <input type="text" name="pengarang" id="pengarang" data-form-field="pengarang" placeholder="Masukkan Nama Pengarang" class="span8">
+                        <input type="text" name="pengarang" id="pengarang" placeholder="Masukkan Nama Pengarang" class="span8">
                     </div>
                 </div>
 
                 <div class="control-group">
                     <label class="control-label">Tahun Terbit</label>
                     <div class="controls">
-                        <input type="text" name="tahun_terbit" id="tahun_terbit" data-form-field="tahun_terbit" placeholder="Masukkan Tahun Terbit" class="span8">
+                        <input type="text" name="tahun_terbit" id="tahun_terbit" placeholder="Masukkan Tahun Terbit" class="span8">
                     </div>
                 </div>
 
                 <div class="control-group">
-                    <label class="control-label" for="basicinput">Kategori</label>
+                    <label class="control-label" for="kategori_id">Kategori</label>
                     <div class="controls">
-                        <select tabindex="1" name="kategori_id" id="kategori_id" data-form-field="kategori_id" data-placeholder="Select kategori.." class="span8">
+                        <select tabindex="1" name="kategori_id" id="kategori_id" data-placeholder="Select kategori.." class="span8">
                             @foreach ($kategori_list as $kategori)
                             <option value="{{ $kategori->id }}">{{ $kategori->kategori }}</option>
                             @endforeach
                         </select>
-                    </div>
-                </div>
-
-
-                <div class="control-group">
-                    <label class="control-label">Stok Buku</label>
-                    <div class="controls">
-                        <input type="text" name="stok" id="stok" data-form-field="stok" placeholder="Masukkan Stok Buku" class="span8">
                     </div>
                 </div>
 
@@ -73,20 +65,13 @@
                     </div>
                 </div>
 
-                {{-- <div class="control-group">
-                    <label class="control-label">Stok</label>
+                <div class="control-group">
+                    <label class="control-label">Stok Buku</label>
                     <div class="controls">
-                        <input type="number" id="stok" data-form-field="image" placeholder="How many issues are there?" class="span8">
+                        <input type="text" name="stok" id="stok" placeholder="Masukkan Stok Buku" class="span8">
                     </div>
                 </div>
-                
 
-                <div class="control-group">
-                    <label class="control-label">Tersedia</label>
-                    <div class="controls">
-                        <input type="number" id="tersedia" data-form-field="tersedia" placeholder="How many issues are there?" class="span8">
-                    </div>
-                </div> --}}
                 <div class="control-group">
                     <div class="controls">
                         <button type="submit" class="btn btn-inverse" id="addbooks">Add Books</button>
@@ -99,8 +84,4 @@
 @stop
 
 @section('custom_bottom_script')
-
-<!-- <script type="text/javascript" src="{{ asset('static/custom/js/script.addbook.js') }}"></script> -->
-
-
 @stop
