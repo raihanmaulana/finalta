c64566d8289ebe785df6a4e487f24d39fd38c4b1
raihanmaulana <raihanmaulana09@gmail.com>
2024-03-16 04:35:08 +0700

    tolak

--- app/Http/Controllers/PeminjamanBukuController.php
+++ app/Http/Controllers/PeminjamanBukuController.php
@@ -97,13 +97,16 @@ class PeminjamanBukuController extends Controller
     {
         $peminjaman = PeminjamanBuku::findOrFail($id);
         if ($peminjaman->status == 0) {
-
             Mail::to($peminjaman->anggota->email)->send(new PeminjamanDitolak($peminjaman));
             $peminjaman->delete();
-
             return redirect()->route('admin.peminjaman.daftar')->with('success', 'Permintaan peminjaman ditolak.');
+        } else {
+            // Tambahkan respons yang sesuai jika peminjaman tidak memiliki status 0
+            // Misalnya, Anda bisa mengarahkan pengguna ke halaman lain atau memberikan pesan yang sesuai
+            return redirect()->route('admin.peminjaman.daftar')->with('error', 'Peminjaman tidak dapat ditolak karena sudah disetujui atau ditolak sebelumnya.');
         }
     }
+
 
 
     public function bukuDipinjam()
--- tests/Feature/tolakPeminjamanTest.php
+++ tests/Feature/tolakPeminjamanTest.php
@@ -5,18 +5,49 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\PeminjamanBuku;
+use App\Models\Buku;
+use App\Mail\PeminjamanDitolak;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class tolakPeminjamanTest extends TestCase
 {
+
+    use DatabaseTransactions;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_tolakPeminjaman()
     {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+        // Membuat pengguna untuk digunakan dalam pengujian
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        Mail::fake();
+
+        // Membuat peminjaman buku dengan status 0
+        $peminjaman = PeminjamanBuku::factory()->create(['status' => 0]);
+
+        // Menjalankan fungsi tolakPeminjaman untuk peminjaman yang dibuat
+        $response = $this->put("/peminjaman/{$peminjaman->id}/tolakPeminjaman");
+
+        // Memeriksa bahwa respons adalah redirect
+        $response->assertStatus(302);
+
+        // Memeriksa bahwa peminjaman berhasil dihapus dari database
+        $this->assertDatabaseMissing('peminjaman_buku', ['id' => $peminjaman->id]);
+
+        // Memeriksa bahwa email pemberitahuan pengembalian ditolak dikirim
+        Mail::assertSent(PeminjamanDitolak::class, function ($mail) use ($peminjaman) {
+            return $mail->hasTo($peminjaman->anggota->email);
+        });
+
+        // Memeriksa bahwa sesi flash memiliki pesan yang sesuai
+        $response->assertSessionHas('success', 'Permintaan peminjaman ditolak.');
     }
 }
